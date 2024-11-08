b0bddb8d10fe31f390e4f5f3d7c0869664a7fa37
WashiArnob <abuwashi.arnob7275924@gmail.com>
2024-11-08 21:27:18 +0600

    first commit

---
+++ lib/screen/onboarding/SplashScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class splashScreen extends StatefulWidget {
+  const splashScreen({super.key});
+
+  @override
+  State<splashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<splashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
