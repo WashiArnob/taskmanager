04190d89f8db78484d4cd7b29242b693067eb065
WashiArnob <abuwashi.arnob7275924@gmail.com>
2024-11-09 09:11:33 +0600

    2nd commit

--- lib/screen/onboarding/SplashScreen.dart
+++ lib/screen/onboarding/SplashScreen.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/svg.dart';
+import 'package:taskmanager/style/style.dart';
 
 class splashScreen extends StatefulWidget {
-  const splashScreen({super.key});
+  const splashScreen({Key? key}) : super(key: key);
 
   @override
-  State<splashScreen> createState() => _SplashScreenState();
+  State<splashScreen> createState() => _splashScreenState();
 }
 
-class _SplashScreenState extends State<splashScreen> {
+class _splashScreenState extends State<splashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(context),
+          Container(
+            padding: EdgeInsets.all(30),
+            child: Center(
+                child: SvgPicture.asset("assets/images/logo.svg",alignment: Alignment.center)
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
