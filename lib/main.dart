ead86d443fd5040c78ae35b520e3b59f1a75a02a
WashiArnob <abuwashi.arnob7275924@gmail.com>
2024-11-16 21:30:04 +0600

    final commit for GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,86 @@
+///Without GetX
+// import 'package:flutter/material.dart';
+// import 'package:taskmanager/screen/onboarding/emailVerificationScreen.dart';
+// import 'package:taskmanager/screen/onboarding/loginScreen.dart';
+// import 'package:taskmanager/screen/onboarding/pinVerificationScreen.dart';
+// import 'package:taskmanager/screen/onboarding/registrationScreen.dart';
+// import 'package:taskmanager/screen/onboarding/setPasswordScreen.dart';
+// import 'package:taskmanager/screen/task/homeScreen.dart';
+// import 'screen/task/taskCreateScreen.dart';
+// import 'utility/utility.dart';
+//
+// main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   String? token= await ReadUserData('id');
+//   if(token==null){
+//     runApp( MyApp("/login"));
+//   }
+//   else{
+//     runApp( MyApp("/"));
+//   }
+// }
+//
+// class MyApp extends StatelessWidget {
+//   final String FirstRoute;
+//   MyApp(this.FirstRoute);
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       title: "Task Manager",
+//       initialRoute: FirstRoute,
+//       routes: {
+//         '/':(context)=> homeScreen(),
+//         '/login':(context)=> loginScreen(),
+//         '/registration':(context)=> registrationScreen(),
+//         '/emailVerification':(context)=> emailVerificationScreen(),
+//         '/pinVerification':(context)=> pinVerificationScreen(),
+//         '/setPassword':(context)=> setPasswordScreen(),
+//         '/taskCreate':(context)=> taskCreateScreen()
+//       },
+//     );
+//   }
+// }
+
+
+///WithGetX
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:taskmanager/screen/onboarding/emailVerificationScreen.dart';
 import 'package:taskmanager/screen/onboarding/loginScreen.dart';
 import 'package:taskmanager/screen/onboarding/pinVerificationScreen.dart';
 import 'package:taskmanager/screen/onboarding/registrationScreen.dart';
 import 'package:taskmanager/screen/onboarding/setPasswordScreen.dart';
 import 'package:taskmanager/screen/task/homeScreen.dart';
-import 'screen/task/taskCreateScreen.dart';
-import 'utility/utility.dart';
+import 'package:taskmanager/screen/task/taskCreateScreen.dart';
+import 'package:taskmanager/utility/utility.dart';
 
-main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  String? token= await ReadUserData('id');
-  if(token==null){
-    runApp( MyApp("/login"));
-  }
-  else{
-    runApp( MyApp("/"));
-  } 
+  String? token = await ReadUserData('id');
+  runApp(MyApp(initialRoute: token == null ? '/login' : '/'));
 }
 
 class MyApp extends StatelessWidget {
-  final String FirstRoute;
-  MyApp(this.FirstRoute);
+  final String initialRoute;
+
+  MyApp({required this.initialRoute});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Task Manager",
-      initialRoute: FirstRoute,
-      routes: {
-        '/':(context)=> homeScreen(),
-        '/login':(context)=> loginScreen(),
-        '/registration':(context)=> registrationScreen(),
-        '/emailVerification':(context)=> emailVerificationScreen(),
-        '/pinVerification':(context)=> pinVerificationScreen(),
-        '/setPassword':(context)=> setPasswordScreen(),
-        '/taskCreate':(context)=> taskCreateScreen()
-      },
+      initialRoute: initialRoute,
+      getPages: [
+        GetPage(name: '/', page: () => homeScreen()),
+        GetPage(name: '/login', page: () => loginScreen()),
+        GetPage(name: '/registration', page: () => registrationScreen()),
+        GetPage(name: '/emailVerification', page: () => emailVerificationScreen()),
+        GetPage(name: '/pinVerification', page: () => pinVerificationScreen()),
+        GetPage(name: '/setPassword', page: () => setPasswordScreen()),
+        GetPage(name: '/taskCreate', page: () => taskCreateScreen()),
+      ],
     );
   }
 }
